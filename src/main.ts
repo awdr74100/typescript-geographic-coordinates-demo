17987a3348f6a5ccb1cdc51f716d2181599a528c
Yi-Ru Lan <a78945612385238@gmail.com>
2022-09-29 21:05:57 +0800

    Getting User Input

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 import '@/main.css';
+
+const form = document.querySelector<HTMLFormElement>('#form')!;
+const addressInput = document.querySelector<HTMLInputElement>('#address')!;
+
+function searchAddressHandler(e: SubmitEvent) {
+  e.preventDefault();
+
+  const enteredAddress = addressInput.value;
+
+  // send this to Google's API
+}
+
+form.addEventListener('submit', searchAddressHandler);
