cbe8e681aacdd20f6d33181ba30873c135ef55a8
Yi-Ru Lan <a78945612385238@gmail.com>
2022-09-30 19:35:55 +0800

    Using nominatim apis

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,40 @@
 import '@/main.css';
+import axios from 'axios';
 
 const form = document.querySelector<HTMLFormElement>('#form')!;
 const addressInput = document.querySelector<HTMLInputElement>('#address')!;
 
-function searchAddressHandler(e: SubmitEvent) {
+type NominatimGeocodingResponse = {
+  features: {
+    geometry: {
+      coordinates: [number, number];
+    };
+  }[];
+};
+
+async function searchAddressHandler(e: SubmitEvent) {
   e.preventDefault();
 
   const enteredAddress = addressInput.value;
+
+  if (!enteredAddress) return;
+
+  try {
+    const { data } = await axios.get<NominatimGeocodingResponse>(
+      `https://nominatim.openstreetmap.org/search?q=${encodeURI(
+        enteredAddress,
+      )}&format=geocodejson`,
+    );
+
+    if (!data.features.length) throw new Error('Could not fetch location!');
+
+    const [longitude, latitude] = data.features[0].geometry.coordinates;
+
+    console.log(longitude, latitude);
+  } catch (error: any) {
+    alert(error.message);
+    console.log(error);
+  }
 
   // send this to Google's API
 }
